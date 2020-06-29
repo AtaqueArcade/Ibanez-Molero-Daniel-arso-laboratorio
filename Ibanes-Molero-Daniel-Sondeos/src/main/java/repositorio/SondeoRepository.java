6213e508c7ac26c462456614dbea22c57d19884f
AtaqueArcade <ataquearcade@gmail.com>
2020-06-30 00:11:15 +0200

    Sondeos conectada con la base de usuarios

--- Ibanes-Molero-Daniel-Sondeos/src/main/java/repositorio/SondeoRepository.java
+++ Ibanes-Molero-Daniel-Sondeos/src/main/java/repositorio/SondeoRepository.java
@@ -15,6 +15,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
+import tipos.Entrada;
+
 public class SondeoRepository {
 	private static SondeoRepository instance;
 	private MongoCollection<Document> sondeos;
@@ -28,31 +30,47 @@ public class SondeoRepository {
 
 	private SondeoRepository() {
 		MongoClient mongoClient = MongoClients.create(
-				"mongodb+srv://ataquearcade:cNyajG9zxdHm2RE@arso-vaorn.mongodb.net/test?retryWrites=true&w=majority");
+				"mongodb+srv://ataquearcade:huWAN4jGusPRjqV@arso-vaorn.mongodb.net/test?retryWrites=true&w=majority");
 		MongoDatabase database = mongoClient.getDatabase("ArSo");
 		this.sondeos = database.getCollection("sondeos");
 	}
 
-	public String saveSondeo(String pregunta, List<String> respuestas, String instrucciones, LocalDateTime apertura,
-			LocalDateTime cierre, int maxSeleccion, int minSeleccion, String visibilidad) {
+	public String saveSondeo(String usuario, String pregunta, List<String> respuestas, String instrucciones,
+			LocalDateTime apertura, LocalDateTime cierre, int maxSeleccion, int minSeleccion, String visibilidad) {
 		Document doc = new Document();
+		doc.append("correo", usuario);
 		doc.append("pregunta", pregunta);
-		doc.append("respuestas", respuestas.toString());
+		if (respuestas != null)
+			doc.append("respuestas", respuestas.toString());
+		else
+			doc.append("respuestas", "");
 		doc.append("instrucciones", instrucciones);
 		doc.append("apertura", apertura.toString());
 		doc.append("cierre", cierre.toString());
 		doc.append("maxSeleccion", maxSeleccion);
 		doc.append("minSeleccion", minSeleccion);
 		doc.append("visibilidad", visibilidad);
+		doc.append("entradas", "");
 		sondeos.insertOne(doc);
 		return ((ObjectId) doc.get("_id")).toHexString();
 	}
 
-	public boolean updateSondeo(String id, List<String> respuestas) {
+	public boolean updateRespuestas(String id, List<String> respuestas) {
 		Document doc = sondeos.find(Filters.eq("_id", new ObjectId(id))).first();
 		if (doc != null)
 			return (doc.replace("respuestas", respuestas) != null);
 		return false;
+	}
+
+	public boolean addEntrada(String id, Entrada e) {
+		Document doc = sondeos.find(Filters.eq("_id", new ObjectId(id))).first();
+		JsonArrayBuilder entradas = Json.createArrayBuilder();
+		if (doc != null) {
+			parseEntradas(doc.getString("entradas")).forEach(entrada -> entradas.add(entrada));
+			entradas.add(parseEntrada(e));
+			System.out.print(entradas.build());
+		}
+		return (doc.replace("entradas", entradas.build()) != null);
 	}
 
 	public JsonObject getSondeo(String id) {
@@ -60,11 +78,12 @@ public class SondeoRepository {
 		JsonArrayBuilder respuestas = Json.createArrayBuilder();
 		parseRespuestas(doc.getString("respuestas")).forEach(respuesta -> respuestas.add(respuesta));
 
-		JsonObject sondeo = Json.createObjectBuilder().add("pregunta", doc.getString("pregunta"))
-				.add("respuestas", respuestas.build()).add("instrucciones", doc.getString("instrucciones"))
-				.add("apertura", doc.getString("apertura")).add("cierre", doc.getString("cierre"))
-				.add("maxSeleccion", doc.getInteger("maxSeleccion")).add("minSeleccion", doc.getInteger("minSeleccion"))
-				.add("visibilidad", doc.getString("visibilidad")).build();
+		JsonObject sondeo = Json.createObjectBuilder().add("correo", doc.getString("correo"))
+				.add("pregunta", doc.getString("pregunta")).add("respuestas", respuestas.build())
+				.add("instrucciones", doc.getString("instrucciones")).add("apertura", doc.getString("apertura"))
+				.add("cierre", doc.getString("cierre")).add("maxSeleccion", doc.getInteger("maxSeleccion"))
+				.add("minSeleccion", doc.getInteger("minSeleccion")).add("visibilidad", doc.getString("visibilidad"))
+				.build();
 		return sondeo;
 	}
 
@@ -76,8 +95,18 @@ public class SondeoRepository {
 		sondeos.deleteMany(new Document());
 	}
 
+	// Supporting methods
 	private List<String> parseRespuestas(String s) {
 		List<String> list = Arrays.asList(s.substring(1, s.length() - 1).split(", "));
 		return list;
+	}
+
+	private List<String> parseEntradas(String s) {
+		List<String> list = Arrays.asList(s.substring(1, s.length() - 1).split(", "));
+		return list;
+	}
+
+	private String parseEntrada(Entrada e) {
+		return e.getCorreo() + ";" + e.getSeleccion();
 	}
 }
--- Ibanes-Molero-Daniel-Sondeos/src/main/java/rest/TratamientoSondeoException.java
+++ Ibanes-Molero-Daniel-Sondeos/src/main/java/rest/TratamientoSondeoException.java
@@ -7,7 +7,7 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class TratamientoSondeoException implements ExceptionMapper<SondeoException> {
 	@Override
-	public Response toResponse(SondeoException arg0) {
-		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(arg0.getMessage()).build();
+	public Response toResponse(SondeoException exception) {
+		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(exception.getMessage()).build();
 	}
 }
