4b7bd032a1694af81b7d4d9f02f8c3c650719e76
AtaqueArcade <ataquearcade@gmail.com>
2020-07-02 00:46:44 +0200

    Reorganización de git

--- Ibanez-Molero-Daniel-Apuntate/Controllers/HomeController.cs
+++ Ibanez-Molero-Daniel-Apuntate/Controllers/HomeController.cs
@@ -1,37 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Ibanez_Molero_Daniel_Apuntate.Models;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Ibanez_Molero_Daniel_Apuntate.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public ActionResult Index()
         {
-            _logger = logger;
-        }
+            ViewBag.Title = "Home Page";
 
-        public IActionResult Index()
-        {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
