c946431b0de057217e90dc51fd3971ed51dfd839
AtaqueArcade <ataquearcade@gmail.com>
2020-07-01 10:30:01 +0200

    Comienzo del tutorial

---
+++ Ibanez-Molero-Daniel-Apuntate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ibanez_Molero_Daniel_Apuntate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
